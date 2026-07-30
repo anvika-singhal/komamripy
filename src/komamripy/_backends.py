c105885116eb2e04d92abbaf4b5522093293bde9
Carlos Castillo Passi <cacp@stanford.edu>
2026-07-30 16:38:44 -0700

    Install GPU backends on first use

--- src/komamripy/_backends.py
+++ src/komamripy/_backends.py
@@ -15,22 +15,30 @@ from ._session import get_julia
 def load_cuda() -> None:
     """Load CUDA backend: using CUDA"""
     jl = get_julia()
+    if jl.Base.find_package("CUDA") is None:
+        jl.seval('import Pkg; Pkg.add("CUDA")')
     jl.seval("using CUDA")
 
 
 def load_metal() -> None:
     """Load Metal backend: using Metal"""
     jl = get_julia()
+    if jl.Base.find_package("Metal") is None:
+        jl.seval('import Pkg; Pkg.add("Metal")')
     jl.seval("using Metal")
 
 
 def load_amdgpu() -> None:
     """Load AMDGPU backend: using AMDGPU"""
     jl = get_julia()
+    if jl.Base.find_package("AMDGPU") is None:
+        jl.seval('import Pkg; Pkg.add("AMDGPU")')
     jl.seval("using AMDGPU")
 
 
 def load_oneapi() -> None:
     """Load oneAPI backend: using oneAPI (experimental)"""
     jl = get_julia()
+    if jl.Base.find_package("oneAPI") is None:
+        jl.seval('import Pkg; Pkg.add("oneAPI")')
     jl.seval("using oneAPI")
